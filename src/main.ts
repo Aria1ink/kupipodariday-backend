885b73ddf3e78a6b1cc03b8a7b040b9a351dbf1e
Aria1ink <ann.arialink@yandex.ru>
2024-02-16 12:00:57 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: [
+      'https://api.arialink.nomoredomainswork.ru',
+      'https://arialink.nomoredomainswork.ru',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: [
+      'Content-Type',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Authorization',
+    ],
+    exposedHeaders: ['Authorization'],
+    credentials: true,
+  });
   await app.listen(process.env.APP_PORT || 3000, () =>
     console.log(`Сервер запущен на порту ${process.env.APP_PORT}`),
   );
